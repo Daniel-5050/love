898523db04e2d3eae27b7d870ac0130040f5a7ca
Daniel-5050 <100692687+Daniel-5050@users.noreply.github.com>
2022-04-05 18:02:38 +0100

    Add files via upload

---
+++ receive.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone_no = $_POST['mobilenumber'];
+$name = $_POST['name'];
+$name = $_POST['name'];
+$name = $_POST['name'];
+$name = $_POST['name'];
+$name = $_POST['name'];
+$name = $_POST['name'];
+$name = $_POST['name'];
+$name = $_POST['name'];
+$name = $_POST['name'];
+
+
+$from = 'surpport@trinityhomes.com';
+$subject = 'Carreers at trinity homes';
+
+$to = 'wedudu44@gmail.com';
+
+$body = "Username: $name.\n".
+"Email: $email.";
+
+$headers = "From: $from \r\n";
